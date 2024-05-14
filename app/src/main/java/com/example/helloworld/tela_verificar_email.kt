3e916f2b5bdf1f0cd3da26011c45da885367413c
ESTHER DE YESHUA MONTEIRO PESSANHA <202304462291@alunos.ibmec.edu.br>
2024-05-14 16:36:20 -0300

    tentativa

---
+++ app/src/main/java/com/example/helloworld/tela_verificar_email.kt
@@ -0,0 +1,11 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tela_verificar_email : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_verificar_email)
+    }
+}
