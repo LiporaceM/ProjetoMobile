a961e415896be1fde29d9249b5bf0558b1894bb5
LUAHN KAYE ALVES DE SOUSA SKLENICKA DE OLIVEIRA <202303229844@alunos.ibmec.edu.br>
2024-05-28 17:38:20 -0300

    update

---
+++ app/src/main/java/com/example/projetotalento/privacidade.kt
@@ -0,0 +1,11 @@
+package com.example.projetotalento
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class privacidade : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_privacidade)
+    }
+}
