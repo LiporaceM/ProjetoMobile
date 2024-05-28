b064c4d3064f0488a18a001ec69bb04913232f4a
ESTHER DE YESHUA MONTEIRO PESSANHA <202304462291@alunos.ibmec.edu.br>
2024-05-28 17:27:03 -0300

    amanda mudou tudo

--- app/src/main/java/com/example/projetotalento/Configuracoes.kt
+++ app/src/main/java/com/example/projetotalento/Configuracoes.kt
@@ -1,11 +1,11 @@
 package com.example.projetotalento
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class Notificacoes : AppCompatActivity() {
+class Configuracoes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notificacoes)
+        setContentView(R.layout.activity_configuracoes)
     }
 }
--- app/src/main/java/com/example/projetotalento/MainActivity.kt
+++ app/src/main/java/com/example/projetotalento/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.projetotalento
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,13 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val logo = findViewById<ImageButton>(R.id.imageButton1)
+
+        logo.setOnClickListener {
+            val intent = Intent(this@MainActivity, Login::class.java)
+            startActivity(intent)
         }
     }
 }
