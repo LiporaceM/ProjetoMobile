aa958564d94da0d16009d4292d228454f5237d99
Raphael S T Meres <raphaelmeres@gmail.com>
2024-06-10 23:29:17 -0300

    add jp

---
+++ app/src/main/java/com/example/projetotalento/TelaAlterarPerfil.kt
@@ -0,0 +1,13 @@
+package com.example.projetotalento
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TelaAlterarPerfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
